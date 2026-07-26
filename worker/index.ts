e9e359a712b8c0bad62a2509501b70c9c0fa349e
jpk0705 <jpkwork0705@gmail.com>
2026-07-26 16:58:36 -0700

    Keep Managed Payments checkout in USD

--- worker/index.ts
+++ worker/index.ts
@@ -298,6 +298,7 @@ const createCheckoutSession = async (env: Env) => {
   }
 
   const parameters = new URLSearchParams({
+    "adaptive_pricing[enabled]": "false",
     "allow_promotion_codes": "false",
     "billing_address_collection": "auto",
     "cancel_url": `${env.PUBLIC_SITE_ORIGIN}/#pricing`,
