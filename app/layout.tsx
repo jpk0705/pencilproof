bd2659c082d10dc440627840366f4e5e170f2346
jpk0705 <jpkwork0705@gmail.com>
2026-08-02 17:01:39 -0700

    Use bundled Roboto for site UI
    * Use bundled Roboto for site UI
    
    * Use bundled Roboto for site UI
    
    * Use bundled Roboto for site UI
    
    * Use bundled Roboto for site UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
 import AnalyticsBoot from "@/app/components/AnalyticsBoot";
 
 const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
